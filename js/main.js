7da8efd090b012025dc30955a076f75bccbd5520
Théo <theo_38180@live.fr>
2021-01-06 13:38:12 +0100

    localstorage ok et le reste yolo

--- js/main.js
+++ js/main.js
@@ -5,15 +5,75 @@ const form = document.querySelector("form");
 const formButton = document.querySelector("button");
 const nom = document.querySelector("#nom");
 const prenom = document.querySelector("#prenom");
-const iFrame = document.querySelector("iframe");
+//pour la partie localstorage
+const sectionFast = document.querySelector(".section-fast");
+const sectionFastButton = document.querySelector(".section-fast button");
 
+const iFrame = document.querySelector("iframe");
 const edtPage = document.querySelector(".edt-page");
 const overlay = document.querySelector(".overlay");
 const navButton = document.querySelector("nav");
 const tip = document.querySelector(".tip");
 
+let valeurNom;
+let valeurPrenom;
+
+const capitalizeFirstLetter = (string) => {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+};
+
+const checkLocalStorage = () => {
+  //Detecte si l'utilisateur avait déjà rentré une ville auparavent, si ce n'est pas le cas, alors le SplashScreen s'affiche
+  if (
+    localStorage.getItem("nom") == "" ||
+    localStorage.getItem("nom") == undefined ||
+    localStorage.getItem("prenom") == "" ||
+    localStorage.getItem("prenom") == undefined
+  ) {
+    sectionFast.classList.add("section-hide");
+  } else {
+    //Lancement du site
+    valeurNom = localStorage.getItem("nom");
+    valeurPrenom = localStorage.getItem("prenom");
+    sectionFastButton.innerHTML = `Accès rapide - en tant que ${capitalizeFirstLetter(
+      valeurPrenom
+    )} ${capitalizeFirstLetter(valeurNom)}`;
+    sectionFast.classList.remove("section-hide");
+  }
+};
+
+checkLocalStorage();
+
 //*VARIABLES DE BASE
 let date = new Date();
+let heure = date.getHours();
+
+//*Pour le lol
+const hoursMsg = (heure) => {
+  let msg = "";
+  if (heure < 7) {
+    msg = "Il se fait tard non ?";
+  } else if (heure < 8) {
+    msg = "Le réveil était pas trop dur ? Courage pour cette journée";
+  } else if (heure < 10) {
+    msg = "J'espère que le cours est pas trop chiant";
+  } else if (heure < 12) {
+    msg = "Courage, c'est bientôt l'heure du repas";
+  } else if (heure < 13) {
+    msg = "Bon 'app !";
+  } else if (heure < 16) {
+    msg = "Courage pour cet aprem";
+  } else if (heure < 18) {
+    msg = "Courage, la journée est bientôt finie";
+  } else if (heure < 22) {
+    msg = "Reposez-vous au lieu de regarder l'edt";
+  } else {
+    msg = "Bonne soirée ~";
+  }
+
+  return msg;
+};
+
 const tipTab = [
   "Désolé si ça met du temps, c'est la faute de l'edt.",
   "Grosse ambiance non ?",
@@ -26,21 +86,27 @@ const tipTab = [
   "Urgh pourquoi ça met toujours aussi longtemps",
   "Je décline toute responsabilité de code moche",
   "Je sais plus quoi écrire comme message de chargement",
+  "Sur Vénus, les jours sont plus long que les années.",
+  'Les différents types de couches présentes dans une étoile à neutron sont appelées "Pâtes nucléaires"',
+  `${hoursMsg(heure)}`,
+  "Aled aled aled aled aled aled",
+  "J'ai plus d'idées aled",
 ];
 
 //permet de pas déclancher la fonction de tip quand l'iframe à fini de charger
 let timerOut = false;
+let firstTime = true;
 
 //*FONCTIONS
 
-//*Vérifications et randoù
-function getRandomInt(max) {
+//*Vérifications et random
+const getRandomInt = (max) => {
   return Math.floor(Math.random() * Math.floor(max));
-}
+};
 
-function isCharacterALetter(char) {
+const isCharacterALetter = (char) => {
   return /[a-zA-Z]/.test(char); //REGEX OUAIS OUAIS OUAIS
-}
+};
 
 //Ouais j'aurais pu les regrouper en une seule fonction mais flemme de me battre contre les EventListener et il est 1h là
 const verifNom = () => {
@@ -59,9 +125,44 @@ const verifPrenom = () => {
 Coeur du progamme, on vérifie si les valeurs des inputs ne sont pas nulles ////comme moi 
 puis on change la source de l'iframe par celle de l'edt de l'utilisateur, puis on affiche la page de l'edt, et on lance la fonction poptip()
 */
+const fastSubmit = () => {
+  edtPage.classList.remove("hide");
+  iFrame.classList.add("hide");
+  overlay.classList.remove("hide");
+
+  if (firstTime) {
+    timerOut = false;
+  }
+  firstTime = false;
+
+  iFrame.src = `https://edtmobiliteng.wigorservices.net//WebPsDyn.aspx?action=posEDTBEECOME&serverid=C&Tel=${valeurPrenom}.${valeurNom}&date=${
+    (date.getMonth() > 8 ? date.getMonth() + 1 : "0" + (date.getMonth() + 1)) +
+    "/" +
+    (date.getDate() > 9 ? date.getDate() : "0" + date.getDate()) +
+    "/" +
+    date.getFullYear()
+  }`;
+
+  popTip();
+};
+
 const submit = function (event) {
   if (prenom.value != "" && nom.value != "") {
-    iFrame.src = `https://edtmobiliteng.wigorservices.net//WebPsDyn.aspx?action=posEDTBEECOME&serverid=C&Tel=${prenom.value.toLowerCase()}.${nom.value.toLowerCase()}&date=${
+    edtPage.classList.remove("hide");
+    iFrame.classList.add("hide");
+    overlay.classList.remove("hide");
+
+    if (firstTime) {
+      timerOut = false;
+    }
+    firstTime = false;
+
+    valeurNom = nom.value.toLowerCase();
+    valeurPrenom = prenom.value.toLowerCase();
+    localStorage.setItem("nom", valeurNom);
+    localStorage.setItem("prenom", valeurPrenom);
+
+    iFrame.src = `https://edtmobiliteng.wigorservices.net//WebPsDyn.aspx?action=posEDTBEECOME&serverid=C&Tel=${valeurPrenom}.${valeurNom}&date=${
       (date.getMonth() > 8
         ? date.getMonth() + 1
         : "0" + (date.getMonth() + 1)) +
@@ -72,8 +173,6 @@ const submit = function (event) {
     }`;
     event.preventDefault();
 
-    edtPage.classList.remove("hide");
-    iFrame.classList.add("hide");
     popTip();
   }
 };
@@ -91,7 +190,6 @@ const popTip = () => {
 
 //Quand l'iframe à fini de charger on enlève l'overlay la
 iFrame.onload = function () {
-  console.log("ok!");
   timerOut = true;
   iFrame.classList.remove("hide");
   overlay.classList.add("hide");
@@ -103,9 +201,11 @@ navButton.addEventListener("click", function () {
   edtPage.classList.add("hide");
   tip.classList.add("hide");
   timerOut = false;
+  checkLocalStorage();
 });
 
 nom.addEventListener("input", verifNom);
 prenom.addEventListener("input", verifPrenom);
 
 form.addEventListener("submit", submit, true);
+sectionFastButton.addEventListener("click", fastSubmit);
